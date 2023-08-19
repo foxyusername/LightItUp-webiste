81e10bb5ab5490b2274cee2f0d094f599902af53
nika <jamaspishvilinika@icloud.com>
2023-08-19 18:51:53 +0400

    changed some things

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // ... other rollup options
-    },
-    outDir: 'dist', // Set your build output directory
-    assetsDir: '', // This should be empty to avoid nested directory structure
-  
-    // Copy additional files to the build directory
-    assetsInclude: ['_redirects'],
-  },
 })
