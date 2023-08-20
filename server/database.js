70b1d4089b658b051c886e10672ace5447086824
nika <jamaspishvilinika@icloud.com>
2023-08-20 11:25:47 +0400

    changed database configuration

--- server/database.js
+++ server/database.js
@@ -1,11 +1,20 @@
 const mysql=require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USERNAME,
     password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DATABASE,
+    ssl: {
+      rejectUnauthorized: true
+    }
+  });
+
+  pool.connect(function(err) {
+    if (err) throw err;
+    console.log("Succesfully connected to PlanetScale!");
+    process.exit(0)
   });
 
 module.exports=pool;
