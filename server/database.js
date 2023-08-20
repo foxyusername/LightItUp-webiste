689aa0adeb8e2c21e1859b0381630b0980d2fb27
nika <jamaspishvilinika@icloud.com>
2023-08-20 15:40:40 +0400

    commit

--- server/database.js
+++ server/database.js
@@ -1,20 +1,11 @@
 const mysql=require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createConnection({
-    host:process.env.DATABASE_HOST,
-    user:process.env.DATABASE_USERNAME,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE,
-    ssl: {
-      rejectUnauthorized: true
-    }
-  });
+const pool = mysql.createConnection(process.env.DATABASE_URL);
 
   pool.connect(function(err) {
     if (err) throw err;
     console.log("Succesfully connected to PlanetScale!");
-    process.exit(0)
   });
 
 module.exports=pool;
